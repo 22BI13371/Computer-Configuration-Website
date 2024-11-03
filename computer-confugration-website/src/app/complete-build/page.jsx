18d96eb94d6d0d5811b21b7b451caeb3450907e4
Nguyen Thanh Long <longnt,22bi13264@usth.edu.vn>
2024-11-03 18:49:54 +0700

    i 'look' ok but still need to fixing

--- computer-confugration-website/src/app/complete-build/page.jsx
+++ computer-confugration-website/src/app/complete-build/page.jsx
@@ -1,8 +1,176 @@
 import React from 'react'
+import '@/styles/completebuild.css'
 
 const completebuild = () => {
   return (
-    <div>Complete builds</div>
+    <>
+      <body>
+      <header class="header">
+          <nav class="navbar">
+              <div className="nav-links">
+                <a href="/">Home</a>
+                <a href="/builder">Builds</a>
+                <a href="/products">Products</a>
+                <a href="/complete-build">Complete Builds</a>
+                <a href="#">Forum</a>
+                <a href="#">Contact</a>
+              </div>
+              <div class="nav-buttons">
+                  <button class="btn">Sign in</button>
+                  <button class="btn">Register</button>
+              </div>
+          </nav>
+      </header>
+
+      <section class="page-title">
+          <h1>Complete PC builds</h1>
+      </section>
+
+      <section class="filter-section">
+          <button class="filter-btn">Filters</button>
+          <input type="text" class="search-input" placeholder="Hinted search text"></input>
+      </section>
+
+      <section class="builds-gallery">
+          <div class="build-item">
+              <img src="pc_build.jpg" alt="PC Build"></img>
+              <div class="build-info">
+                  <h2>Exceptional Black Build with Core i7-12700F</h2>
+                  <p>Intel Core i7-12700F | GeForce RTX 3060 12GB</p>
+                  <p>16GB DDR4 3200 | 500GB SSD</p>
+                  <p>LGA170 Semi-Modular 600W</p>
+                  <button class="btn">Price: $1500</button>
+              </div>
+          </div>
+          <div class="build-item">
+              <img src="pc_build.jpg" alt="PC Build"></img>
+              <div class="build-info">
+                  <h2>Exceptional Black Build with Core i7-12700F</h2>
+                  <p>Intel Core i7-12700F | GeForce RTX 3060 12GB</p>
+                  <p>16GB DDR4 3200 | 500GB SSD</p>
+                  <p>LGA170 Semi-Modular 600W</p>
+                  <button class="btn">Price: $1500</button>
+              </div>
+          </div>
+          <div class="build-item">
+              <img src="pc_build.jpg" alt="PC Build"></img>
+              <div class="build-info">
+                  <h2>Exceptional Black Build with Core i7-12700F</h2>
+                  <p>Intel Core i7-12700F | GeForce RTX 3060 12GB</p>
+                  <p>16GB DDR4 3200 | 500GB SSD</p>
+                  <p>LGA170 Semi-Modular 600W</p>
+                  <button class="btn">Price: $1500</button>
+              </div>
+          </div>
+          <div class="build-item">
+              <img src="pc_build.jpg" alt="PC Build"></img>
+              <div class="build-info">
+                  <h2>Exceptional Black Build with Core i7-12700F</h2>
+                  <p>Intel Core i7-12700F | GeForce RTX 3060 12GB</p>
+                  <p>16GB DDR4 3200 | 500GB SSD</p>
+                  <p>LGA170 Semi-Modular 600W</p>
+                  <button class="btn">Price: $1500</button>
+              </div>
+          </div>
+          <div class="build-item">
+              <img src="pc_build.jpg" alt="PC Build"></img>
+              <div class="build-info">
+                  <h2>Exceptional Black Build with Core i7-12700F</h2>
+                  <p>Intel Core i7-12700F | GeForce RTX 3060 12GB</p>
+                  <p>16GB DDR4 3200 | 500GB SSD</p>
+                  <p>LGA170 Semi-Modular 600W</p>
+                  <button class="btn">Price: $1500</button>
+              </div>
+          </div>
+          <div class="build-item">
+              <img src="pc_build.jpg" alt="PC Build"></img>
+              <div class="build-info">
+                  <h2>Exceptional Black Build with Core i7-12700F</h2>
+                  <p>Intel Core i7-12700F | GeForce RTX 3060 12GB</p>
+                  <p>16GB DDR4 3200 | 500GB SSD</p>
+                  <p>LGA170 Semi-Modular 600W</p>
+                  <button class="btn">Price: $1500</button>
+              </div>
+          </div>
+          <div class="build-item">
+              <img src="pc_build.jpg" alt="PC Build"></img>
+              <div class="build-info">
+                  <h2>Exceptional Black Build with Core i7-12700F</h2>
+                  <p>Intel Core i7-12700F | GeForce RTX 3060 12GB</p>
+                  <p>16GB DDR4 3200 | 500GB SSD</p>
+                  <p>LGA170 Semi-Modular 600W</p>
+                  <button class="btn">Price: $1500</button>
+              </div>
+          </div>
+          <div class="build-item">
+              <img src="pc_build.jpg" alt="PC Build"></img>
+              <div class="build-info">
+                  <h2>Exceptional Black Build with Core i7-12700F</h2>
+                  <p>Intel Core i7-12700F | GeForce RTX 3060 12GB</p>
+                  <p>16GB DDR4 3200 | 500GB SSD</p>
+                  <p>LGA170 Semi-Modular 600W</p>
+                  <button class="btn">Price: $1500</button>
+              </div>
+          </div>
+          <div class="build-item">
+              <img src="pc_build.jpg" alt="PC Build"></img>
+              <div class="build-info">
+                  <h2>Exceptional Black Build with Core i7-12700F</h2>
+                  <p>Intel Core i7-12700F | GeForce RTX 3060 12GB</p>
+                  <p>16GB DDR4 3200 | 500GB SSD</p>
+                  <p>LGA170 Semi-Modular 600W</p>
+                  <button class="btn">Price: $1500</button>
+              </div>
+          </div>
+          <div class="build-item">
+              <img src="pc_build.jpg" alt="PC Build"></img>
+              <div class="build-info">
+                  <h2>Exceptional Black Build with Core i7-12700F</h2>
+                  <p>Intel Core i7-12700F | GeForce RTX 3060 12GB</p>
+                  <p>16GB DDR4 3200 | 500GB SSD</p>
+                  <p>LGA170 Semi-Modular 600W</p>
+                  <button class="btn">Price: $1500</button>
+              </div>
+          </div>
+          <div class="build-item">
+              <img src="pc_build.jpg" alt="PC Build"></img>
+              <div class="build-info">
+                  <h2>Exceptional Black Build with Core i7-12700F</h2>
+                  <p>Intel Core i7-12700F | GeForce RTX 3060 12GB</p>
+                  <p>16GB DDR4 3200 | 500GB SSD</p>
+                  <p>LGA170 Semi-Modular 600W</p>
+                  <button class="btn">Price: $1500</button>
+              </div>
+          </div>
+          <div class="build-item">
+              <img src="pc_build.jpg" alt="PC Build"></img>
+              <div class="build-info">
+                  <h2>Exceptional Black Build with Core i7-12700F</h2>
+                  <p>Intel Core i7-12700F | GeForce RTX 3060 12GB</p>
+                  <p>16GB DDR4 3200 | 500GB SSD</p>
+                  <p>LGA170 Semi-Modular 600W</p>
+                  <button class="btn">Price: $1500</button>
+              </div>
+          </div>
+      </section>
+
+      <div class="pagination">
+          <button class="pagination-btn">Previous</button>
+          <div class="page-numbers">
+              <button class="page-number active">1</button>
+              <button class="page-number">2</button>
+              <button class="page-number">3</button>
+              <span>...</span>
+              <button class="page-number">68</button>
+          </div>
+          <button class="pagination-btn">Next</button>
+      </div>
+
+      <footer class="footer">
+          <p>Thank you for visiting!</p>
+      </footer>
+      </body>    
+    </>
   )
 }
 
