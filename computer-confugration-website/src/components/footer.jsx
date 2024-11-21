ff0ada1e1042ed31a63b609037aab2626a5d3941
Quân Nguyễn <155754824+22BI13371@users.noreply.github.com>
2024-11-21 16:10:17 +0700

    Revert "code for front"

--- computer-confugration-website/src/components/footer.jsx
+++
@@ -1,9 +0,0 @@
-import '@/styles/header.css'
-const Footer = () => {
-    return (
-        <footer className="footer">
-            <p>Thank you for visiting!</p>
-        </footer>
-    )
-}
-export default Footer
