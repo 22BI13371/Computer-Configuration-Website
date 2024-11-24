3e68f466aa7d221312bd71b1981ce0e02b164081
Quân Nguyễn <155754824+22BI13371@users.noreply.github.com>
2024-11-24 10:39:40 +0700

    Disabled seed

--- computer-confugration-website/src/app/seed/route.js
+++ computer-confugration-website/src/app/seed/route.js
@@ -1,48 +1,48 @@
-import bcrypt from 'bcrypt';
-import { db } from '@vercel/postgres';
+// import bcrypt from 'bcrypt';
+// import { db } from '@vercel/postgres';
 
-const client = await db.connect();
+// const client = await db.connect();
 
-async function seedUsers() {
-  await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+// async function seedUsers() {
+//   await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
 
-  await client.sql`
-    CREATE TABLE IF NOT EXIST users (
-      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-      username  VARCHAR(255) NOT NULL,
-      email TEXT NOT NULL UNIQUE,
-      password TEXT NOT NULL,
-      is_admin BOOLEAN NOT NULL DEFAULT false,
-      created_at VARCHAR NOT NULL
-    );
-  `;
-}
+//   await client.sql`
+//     CREATE TABLE IF NOT EXIST users (
+//       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+//       username  VARCHAR(255) NOT NULL,
+//       email TEXT NOT NULL UNIQUE,
+//       password TEXT NOT NULL,
+//       is_admin BOOLEAN NOT NULL DEFAULT false,
+//       created_at VARCHAR NOT NULLs
+//     );
+//   `;
+// }
 
-async function seedPosts() {
-  await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+// async function seedPosts() {
+//   await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
 
-  await client.sql`
-    CREATE TABLE IF NOT EXIST posts (
-      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-      user_id UUID NOT NULL,
-      title TEXT NOT NULL,
-      body TEXT NOT NULL,
-      created_at VARCHAR NOT NULL,
-      updated_at VARCHAR
-    );
-  `;
-}
+//   await client.sql`
+//     CREATE TABLE IF NOT EXIST posts (
+//       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+//       user_id UUID NOT NULL,
+//       title TEXT NOT NULL,
+//       body TEXT NOT NULL,
+//       created_at VARCHAR NOT NULL,
+//       updated_at VARCHAR
+//     );
+//   `;
+// }
 
-async function seedComments() {
-  await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+// async function seedComments() {
+//   await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
 
-  await client.sql`
-    CREATE TABLE IF NOT EXIST comments (
-      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-      post_id UUID NOT NULL,
-      user_id UUID NOT NULL,
-      text TEXT NOT NULL,
-      created_at VARCHAR
-    );
-  `;
-}
+//   await client.sql`
+//     CREATE TABLE IF NOT EXIST comments (
+//       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+//       post_id UUID NOT NULL,
+//       user_id UUID NOT NULL,
+//       text TEXT NOT NULL,
+//       created_at VARCHAR
+//     );
+//   `;
+// }
