fbb6177ad7a121aeee3db32c404dbfd55685e788
VuTuanMinh22bi13317 <hkpotatoman@gmail.com>
2024-11-28 15:25:15 +0700

    filter UI

--- computer-confugration-website/src/app/page.js
+++ computer-confugration-website/src/app/page.js
@@ -6,8 +6,9 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div>
-          <Link href={"/login"}>login</Link>
+          <Link href={"/filter"}>filter</Link>
         </div>
+
         <Image
           className="dark:invert"
           src="https://nextjs.org/icons/next.svg"
