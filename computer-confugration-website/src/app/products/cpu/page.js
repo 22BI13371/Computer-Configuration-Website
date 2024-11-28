eaad1b88ed3462fd266e19df805225c16397bd8a
QuanChuHong269 <chuhongquan269@gmail.com>
2024-11-28 20:02:40 +0700

    add dropbox for product button and cpu product page

---
+++ computer-confugration-website/src/app/products/cpu/page.js
@@ -0,0 +1,11 @@
+import CPU from './cpu';
+
+const CPUPage = () => {
+    return (
+        <div>
+            <CPU /> {/* Render the CPU component */}
+        </div>
+    );
+};
+
+export default CPUPage;
