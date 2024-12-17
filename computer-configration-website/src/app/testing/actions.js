f6dc7904cac0c926bfb83bf747e77d873b08c57f
Quân Nguyễn <155754824+22BI13371@users.noreply.github.com>
2024-12-17 19:15:14 +0700

    testing cookie

---
+++ computer-configration-website/src/app/testing/actions.js
@@ -0,0 +1,26 @@
+'use server';
+
+import { cookies } from 'next/headers';
+import { fetchPcPartWithFilter } from '../lib/data';
+
+export async function setCookie(data) {
+  const cookieStore = await cookies();
+  let cpuName = cookieStore.get('cpu_name');
+
+  if (cpuName == '') {
+    let cpus = await fetchPcPartWithFilter(
+      'CPU',
+      {
+        current_price: { min: 44000, max: 46000 },
+        manufacturer: ['AMD', 'Intel'],
+      },
+      { series: 'AMD Ryzen 7' }
+    );
+    // console.log(cpus[0].name);
+
+    cookieStore.set('cpu_name', cpus[0].name);
+  } else {
+    cpuName = cookieStore.get('cpu_name');
+    console.log(cpuName.value);
+  }
+}
--- computer-configration-website/src/app/testing/page.js
+++ computer-configration-website/src/app/testing/page.js
@@ -1,20 +1,31 @@
 import { fetchCpuWithFilter, fetchPcPartWithFilter } from '../lib/data';
+import { cookies } from 'next/headers';
+import { setCookie } from './actions';
 
 export default async function Page() {
+  const cookieStore = await cookies();
+  let cpus;
   // const cpus = await fetchPcParts('Motherboard');
   try {
-    const cpus = await fetchPcPartWithFilter(
-      'CPU',
-      {
-        current_price: { min: 44000, max: 46000 },
-        manufacturer: ['AMD', 'Intel'],
-      },
-      { series: 'AMD Ryzen 7' }
-    );
+    if (!cpus) {
+      cpus = await fetchPcPartWithFilter(
+        'CPU',
+        {
+          current_price: { min: 44000, max: 46000 },
+          manufacturer: ['AMD', 'Intel'],
+        },
+        { series: 'AMD Ryzen 7' }
+      );
+    } else {
+      let cpuName = cookieStore.get('cpu_name');
+    }
 
     return cpus.map((cpu) => (
       <div>
-        <p>{cpu.name}</p>
+        <form action={setCookie}>
+          <button type="submit">click here</button>
+        </form>
+        {/* <p>{cpu.name}</p> */}
         {/* <p>{cpu.current_price / 100}</p>
       <p>{cpu.specification.cpu_socket}</p> */}
       </div>
