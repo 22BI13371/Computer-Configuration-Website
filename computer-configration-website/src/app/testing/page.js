dbc031d1dcdbb36b25c5db45e0d6ab2e09a7183d
Nguyen Thanh Long <longnt,22bi13264@usth.edu.vn>
2024-12-19 13:57:32 +0700

    update

--- computer-configration-website/src/app/testing/page.js
+++ computer-configration-website/src/app/testing/page.js
@@ -6,17 +6,17 @@ export default async function Page() {
     const cpus = await fetchPcPartWithFilter(
       'CPU',
       {
-        current_price: { min: 44000, max: 46000 },
-        manufacturer: ['AMD', 'Intel'],
+        current_price: { min: 42000, max: 45000 },
+        manufacturer: ['Intel', 'AMD'],
       },
-      { series: 'AMD Ryzen 7' }
+      { series: 'Intel Core i9' }
     );
-
+    console.log(cpus);
     return cpus.map((cpu) => (
       <div>
         <p>{cpu.name}</p>
-        {/* <p>{cpu.current_price / 100}</p>
-      <p>{cpu.specification.cpu_socket}</p> */}
+        <p>{cpu.current_price}</p>
+        <p>{cpu.part_number}</p>
       </div>
     ));
   } catch (error) {
