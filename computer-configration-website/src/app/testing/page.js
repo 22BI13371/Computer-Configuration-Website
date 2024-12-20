180b078a7cc734cdadcadba84126efd7440077ea
VuTuanMinh22bi13317 <hkpotatoman@gmail.com>
2024-12-20 11:58:18 +0700

    update the filterbar and product page (change base on your like if you want)

--- computer-configration-website/src/app/testing/page.js
+++ computer-configration-website/src/app/testing/page.js
@@ -1,21 +1,21 @@
-import { fetchCpuWithFilter, fetchPcPartWithFilter } from '../lib/data';
+import { fetchCpuWithFilter, fetchPcPartWithFilter } from "../lib/data";
 
 export default async function Page() {
   // const cpus = await fetchPcParts('Motherboard');
   const cpus = await fetchPcPartWithFilter(
-    'CPU',
+    "CPU",
     {
-      current_price: { min: 44000, max: 46000 },
-      manufacturer: ['AMD', 'Intel'],
+      current_price: { min: 10000, max: 46000 },
+      manufacturer: ["Intel", "AMD"],
     },
-    { series: 'AMD Ryzen 7' }
+    { series: "Intel Core i9" }
   );
 
   return cpus.map((cpu) => (
     <div>
       <p>{cpu.name}</p>
-      {/* <p>{cpu.current_price / 100}</p>
-      <p>{cpu.specification.cpu_socket}</p> */}
+       <p>{cpu.current_price / 100}</p>
+      <p>{cpu.specification.cpu_socket}</p> 
     </div>
   ));
 }
