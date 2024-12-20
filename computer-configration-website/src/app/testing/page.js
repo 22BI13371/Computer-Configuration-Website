d7f415a3587f550966bf8de526fe44721f4378a9
VuTuanMinh22bi13317 <hkpotatoman@gmail.com>
2024-12-20 12:03:35 +0700

    update filter bar

--- computer-configration-website/src/app/testing/page.js
+++ computer-configration-website/src/app/testing/page.js
@@ -1,23 +1,60 @@
-import { fetchCpuWithFilter, fetchPcPartWithFilter } from '../lib/data';
+import {
+  fetchCpuWithFilter,
+  fetchPcPartWithFilter,
+  fetchPcBuildPartsWithId,
+} from '../lib/data';
+import { cookies } from 'next/headers';
+import { setCookie, clearCookie } from './actions';
 
 export default async function Page() {
   const cookieStore = await cookies();
   let cpus;
+  let build1_parts;
   // const cpus = await fetchPcParts('Motherboard');
-  const cpus = await fetchPcPartWithFilter(
-    'CPU',
-    {
-      current_price: { min: 44000, max: 46000 },
-      manufacturer: ['AMD', 'Intel'],
-    },
-    { series: 'AMD Ryzen 7' }
-  );
 
-  return cpus.map((cpu) => (
-    <div>
-      <p>{cpu.name}</p>
-      {/* <p>{cpu.current_price / 100}</p>
-      <p>{cpu.specification.cpu_socket}</p> */}
-    </div>
-  ));
+  try {
+    if (!cpus) {
+      cpus = await fetchPcPartWithFilter(
+        'CPU',
+        {
+          current_price: { min: 44000, max: 46000 },
+          manufacturer: ['AMD', 'Intel'],
+        },
+        { series: 'AMD Ryzen 7' }
+      );
+
+      build1_parts = await fetchPcBuildPartsWithId(
+        '565ed09b-94bf-4b7e-9af9-b8ae6aa78624'
+      );
+    } else {
+      let cpuName = cookieStore.get('cpu_name');
+    }
+
+    // return cpus.map((cpu) => (
+    //   <div>
+    //     <form action={setCookie}>
+    //       <button type="submit">click here</button>
+    //       <button type="submit" formAction={clearCookie}>
+    //         delete cookie
+    //       </button>
+    //     </form>
+    //     {/* <p>{cpu.name}</p> */}
+    //     <p>{cpu.current_price / 100}</p>
+    //     <p>{cpu.specification.cpu_socket}</p>
+    //   </div>
+    // ));
+
+    return build1_parts.map((parts) => (
+      <div>
+        <p> {parts.id} </p>
+      </div>
+    ));
+  } catch (error) {
+    console.log(error);
+    return (
+      <div>
+        <p>See this? ur db not up. Fix it </p>
+      </div>
+    );
+  }
 }
